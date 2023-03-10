c1ed501fadb0b786b9cf9e2e762230bcbfd90c8f
DESKTOP-JPP5GVA\Moon <foqlzm12345@gmail.com>
2023-03-11 06:28:09 +0900

    삭제

--- src/main/java/com/myroom/basicUser/BasicUserRestController.java
+++
@@ -1,8 +0,0 @@
-package com.myroom.basicUser;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BasicUserRestController {
-
-}
--- src/main/java/com/myroom/test/TestDAO/TestDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myroom.test.TestDAO;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestDAO {
-
-	public int test();
-}
