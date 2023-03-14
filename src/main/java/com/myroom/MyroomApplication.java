07c4ba5b1ae47c1dc3fd8c501175b28da50463f2
DESKTOP-JPP5GVA\Moon <foqlzm12345@gmail.com>
2023-03-14 21:55:33 +0900

    application 수정

--- src/main/java/com/myroom/MyroomApplication.java
+++ src/main/java/com/myroom/MyroomApplication.java
@@ -2,11 +2,12 @@ package com.myroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class MyroomApplication {
+public class MyroomApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyroomApplication.class, args);
